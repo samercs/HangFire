e0e5d2ba9dfce41f239999d8414d3bb761ee36a3
samercs <samer_mail_2006@yahoo.com>
2017-09-21 22:33:37 +0300

    Add project files.

---
+++ Hangfire02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hangfire02.Startup))]
+namespace Hangfire02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
